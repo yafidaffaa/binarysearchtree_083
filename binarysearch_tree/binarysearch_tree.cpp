40b029e0469959f7300a734ab5b224dabc92eddb
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-07-02 19:18:43 +0700

    membuat newNode dan memasukan valuenya

--- binarysearch_tree/binarysearch_tree.cpp
+++ binarysearch_tree/binarysearch_tree.cpp
@@ -23,4 +23,14 @@ public:
 	BinaryTree() {
 		ROOT = NULL; //Initializing ROOT to NULL
 	}
+
+	void insert(string element) {//Insert a node in the binary search tree
+		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; // Assign value to the data field of the new node
+		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+	}
 };
