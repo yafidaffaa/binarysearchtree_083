b6fb69842113268c6f9402c03a3263f7d6b046fd
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-07-02 19:56:16 +0700

    kondisi untuk mencetak dari kiri-kanan dan node saat ini

--- binarysearch_tree/binarysearch_tree.cpp
+++ binarysearch_tree/binarysearch_tree.cpp
@@ -93,7 +93,18 @@ public:
 	}
 
 	void postorder(Node* ptr) {
-		
+		//performs the postorder traversal of the tree
+		if (ROOT == NULL)
+		{
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << "";
+
+		}
 	}
 
 };
